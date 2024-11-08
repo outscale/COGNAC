10e43ed385e6def4cd9d665ed3ac2750d8181800
Matthias Gatto <matthias.gatto@outscale.com>
2024-11-08 14:25:04 +0100

    add an option to funclist to generate function from paths
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- bin/funclist.c
+++ bin/funclist.c
@@ -1,34 +1,58 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include "json.h"
 
 int main(int ac, char **av)
 {
+	bool funclist_from_path = ac > 2;
 	struct json_object *j_file = json_object_from_file(av[1]);
 
-	struct json_object *compo;
-	struct json_object *schema;
 	int ret = 0;
-	ret = json_object_object_get_ex(j_file, "components", &compo);
-	if (!ret)
-		return 1;
-	json_object_object_get_ex(compo, "schemas", &schema);
-	if (!ret)
-		return 1;
+	int func_ret = 1;
 	int first = 1;
 
-	json_object_object_foreach(schema, k, v_) {
-		char *new_k = strdup(k);
-		char *resquest = strstr(new_k, "Request");
-		if (resquest) {
+	if (!funclist_from_path) {
+		struct json_object *compo;
+		struct json_object *schema;
+
+		ret = json_object_object_get_ex(j_file, "components", &compo);
+		if (!ret)
+			goto err;
+		json_object_object_get_ex(compo, "schemas", &schema);
+		if (!ret)
+			goto err;
+
+		json_object_object_foreach(schema, k, v_) {
+			char *new_k = strdup(k);
+			char *resquest = strstr(new_k, "Request");
+
+			if (resquest) {
+				if (!first)
+					putchar(' ');
+				first = 0;
+				*resquest = 0;
+				printf("%s", new_k);
+			}
+			free(new_k);
+		}
+	} else {
+		struct json_object *paths;
+
+		ret = json_object_object_get_ex(j_file, "paths", &paths);
+		if (!ret)
+			goto err;
+
+		json_object_object_foreach(paths, k, v_) {
 			if (!first)
 				putchar(' ');
 			first = 0;
-			*resquest = 0;
-			printf("%s", new_k);
+			printf("%s", k+1);
 		}
-		free(new_k);
 	}
+
+	func_ret = 0;
+err:
 	json_object_put(j_file);
-	return 0;
+	return func_ret;
 }
