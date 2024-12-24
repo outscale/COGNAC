6f4784b728593b3f60a55bafe2ed2c7cadf01ebb
Matthias Gatto <matthias.gatto@outscale.com>
2024-12-24 15:00:19 +0100

    fix C for old compiler
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- bin/construct_endpoint.c
+++ bin/construct_endpoint.c
@@ -6,21 +6,24 @@ int main(int ac, char **av)
 {
 	int buf_i;
 	char *componant_name = av[1];
-
+	char *brk;
 again:
-	char *brk = strchr(componant_name, '{');
-	if (!brk) {
+	{
+		brk = strchr(componant_name, '{');
+		if (!brk) {
+			printf("\tosc_str_append_string(&e->endpoint, \"%s\");\n", componant_name);
+			return 0;
+		}
+
+		*brk = 0;
 		printf("\tosc_str_append_string(&e->endpoint, \"%s\");\n", componant_name);
-		return 0;
+
+		printf("\tosc_str_append_string(&e->endpoint, e->");
+		for (++brk; *brk != '}'; ++brk)
+			putchar(*brk);
+		puts(");");
+		componant_name = brk + 1;
+		goto again;
 	}
-
-	*brk = 0;
-	printf("\tosc_str_append_string(&e->endpoint, \"%s\");\n", componant_name);
-
-	printf("\tosc_str_append_string(&e->endpoint, e->");
-	for (++brk; *brk != '}'; ++brk)
-		putchar(*brk);
-	puts(");");
-	componant_name = brk + 1;
-	goto again;
+	return 0;
 }
